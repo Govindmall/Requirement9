ca7c64396344aaa9a13e6548dbb97104747ee41a
Govindmall <govind.mall0704@gmail.com>
2025-06-22 11:21:21 +0530

    requirement9

--- src/main/scala/Requirement9.scala
+++ src/main/scala/Requirement9.scala
@@ -7,16 +7,13 @@ object Requirement9{
     //read the csv file into DataFrame
     val salesDF = spark.read.option("header", "true").csv("C:/Users/gomall/Downloads/sparsales.csv")
     //group by country, sum Total Revenue
-    val salesDF1=salesDF.coalesce(2)
+    val salesDF1=salesDF.coalesce(1)
     val countrySalesDF=salesDF1.groupBy("Country").agg(sum("Total Revenue").as("Total_Sales")).persist()
     //find the country with highest total sales
     val topCountryDF=countrySalesDF.orderBy(desc("Total_Sales")).limit(1)
-
     topCountryDF.show()
     topCountryDF.write.parquet("C:/Users/gomall/Desktop/requirement9.parquet")
     spark.stop()
-
-
   }
 
 }
